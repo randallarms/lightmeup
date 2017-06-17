f430ed6393c03ae3a527ec80a04a9f9ae4d8893d
Randall Arms <contact.randallArms@gmail.com>
2017-06-17 02:09:13 -0700

    v1.1 | Added a check for metadata blocks
    - Added a check for metadata blocks

--- src/com/kraken/lightmeup/LightProcessing.java
+++ src/com/kraken/lightmeup/LightProcessing.java
@@ -15,9 +15,78 @@ public class LightProcessing {
 	WeakHashMap<Location, Material> litTypes = new WeakHashMap<Location, Material>();
 	WeakHashMap<Location, Byte> litData = new WeakHashMap<Location, Byte>();
 	
+	ArrayList<Material> noGoMats = new ArrayList<Material>();
+	
   //Constructor
 	public LightProcessing(Main plugin) {
 		this.plugin = plugin;
+		noGoMats.add(Material.AIR);
+		noGoMats.add(Material.SEA_LANTERN);
+		noGoMats.add(Material.CHEST);
+		noGoMats.add(Material.TRAPPED_CHEST);
+		noGoMats.add(Material.SIGN);
+		noGoMats.add(Material.WALL_SIGN);
+		noGoMats.add(Material.BEACON);
+		noGoMats.add(Material.ENCHANTMENT_TABLE);
+		noGoMats.add(Material.WORKBENCH);
+		noGoMats.add(Material.JUKEBOX);
+		noGoMats.add(Material.NOTE_BLOCK);
+		noGoMats.add(Material.ANVIL);
+		noGoMats.add(Material.ARMOR_STAND);
+		noGoMats.add(Material.BANNER);
+		noGoMats.add(Material.BARRIER);
+		noGoMats.add(Material.BEDROCK);
+		noGoMats.add(Material.BED_BLOCK);
+		noGoMats.add(Material.BED);
+		noGoMats.add(Material.BREWING_STAND);
+		noGoMats.add(Material.BURNING_FURNACE);
+		noGoMats.add(Material.CAKE_BLOCK);
+		noGoMats.add(Material.COMMAND);
+		noGoMats.add(Material.COMMAND_CHAIN);
+		noGoMats.add(Material.COMMAND_MINECART);
+		noGoMats.add(Material.COMMAND_REPEATING);
+		noGoMats.add(Material.DAYLIGHT_DETECTOR);
+		noGoMats.add(Material.DAYLIGHT_DETECTOR_INVERTED);
+		noGoMats.add(Material.DIODE);
+		noGoMats.add(Material.DIODE_BLOCK_OFF);
+		noGoMats.add(Material.DIODE_BLOCK_ON);
+		noGoMats.add(Material.DISPENSER);
+		noGoMats.add(Material.DOUBLE_PLANT);
+		noGoMats.add(Material.DROPPER);
+		noGoMats.add(Material.END_CRYSTAL);
+		noGoMats.add(Material.END_GATEWAY);
+		noGoMats.add(Material.ENDER_PORTAL);
+		noGoMats.add(Material.ENDER_PORTAL_FRAME);
+		noGoMats.add(Material.FIRE);
+		noGoMats.add(Material.FURNACE);
+		noGoMats.add(Material.HOPPER);
+		noGoMats.add(Material.HOPPER_MINECART);
+		noGoMats.add(Material.ITEM_FRAME);
+		noGoMats.add(Material.LEVER);
+		noGoMats.add(Material.PAINTING);
+		noGoMats.add(Material.PISTON_BASE);
+		noGoMats.add(Material.PISTON_EXTENSION);
+		noGoMats.add(Material.PISTON_MOVING_PIECE);
+		noGoMats.add(Material.PISTON_STICKY_BASE);
+		noGoMats.add(Material.POWERED_RAIL);
+		noGoMats.add(Material.REDSTONE);
+		noGoMats.add(Material.REDSTONE_BLOCK);
+		noGoMats.add(Material.REDSTONE_COMPARATOR);
+		noGoMats.add(Material.REDSTONE_COMPARATOR_OFF);
+		noGoMats.add(Material.REDSTONE_COMPARATOR_ON);
+		noGoMats.add(Material.REDSTONE_TORCH_ON);
+		noGoMats.add(Material.REDSTONE_TORCH_OFF);
+		noGoMats.add(Material.REDSTONE_WIRE);
+		noGoMats.add(Material.SAPLING);
+		noGoMats.add(Material.SIGN_POST);
+		noGoMats.add(Material.SKULL);
+		noGoMats.add(Material.STANDING_BANNER);
+		noGoMats.add(Material.STORAGE_MINECART);
+		noGoMats.add(Material.STRUCTURE_BLOCK);
+		noGoMats.add(Material.TRIPWIRE);
+		noGoMats.add(Material.TRIPWIRE_HOOK);
+		noGoMats.add(Material.VINE);
+		noGoMats.add(Material.WALL_BANNER);
 	}
 	
   //"light" command processing
@@ -26,7 +95,7 @@ public class LightProcessing {
 		Location underfoot = player.getLocation().add(0, -1, 0);
 		Material mat = underfoot.getBlock().getType();
 		
-		if ( !mat.equals(Material.AIR) && !mat.equals(Material.SEA_LANTERN) ) {
+		if ( !noGoMats.contains(mat) ) {
 			
 			saveBlock(player, underfoot);
 			underfoot.getBlock().setType(Material.SEA_LANTERN);
